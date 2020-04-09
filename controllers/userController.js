29d1387daa24698b3aab0e663a8155fd42266a4e
Jean C. Contreras <jean.cntrs@gmail.com>
2020-04-08 22:50:31 -0400

    Was added password encryption and token creation with jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,15 @@
 const User = require('../models/User');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
 
 exports.createUser = async (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
     const { email, password } = req.body;
 
@@ -10,8 +19,20 @@ exports.createUser = async (req, res) => {
             return res.status(400).json({ msg: 'Usuario ya existe' });
         } else {
             user = new User(req.body);
+            const salt = await bcryptjs.genSalt(10);
+            user.password = await bcryptjs.hash(password, salt);
             await user.save();
-            res.json({ msg: 'Usuario creado exitosamente' });
+            const payload = {
+                user: {
+                    userId: user.id
+                }
+            };
+            jwt.sign(payload, process.env.SECRET, {
+                expiresIn: 3600
+            }, (error, token) => {
+                if (error) throw error;
+                res.json({ token });
+            });
         }
     } catch (error) {
         console.log(error)
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { check } = require('express-validator');
 
-router.post('/', userController.createUser);
+router.post(
+    '/',
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'Agrega un email válido').isEmail(),
+        check('password', 'La contraseña debe tener minimo 6 caracteres').isLength({ min: 6 })
+    ],
+    userController.createUser
+);
 
 module.exports = router;
